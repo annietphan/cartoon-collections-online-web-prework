913b58a93ccdbecf51c2e29c452290628a81c16c
Annie Phan <anniephan.atp@gmail.com>
2019-03-02 18:23:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,11 @@ def long_planeteer_calls(array)# code an argument here
   array.any? {|element| element.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.any?{|item| cheese_types.include?(item)} == false
+    nil
+  else
+    puts "#{item}"
 end
